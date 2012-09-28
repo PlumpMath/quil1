6785ab670e44583f030515bf9f92c7474ec7a33a
juanantonioruz <juanantonioruz@gmail.com>
2012-09-28 11:42:41 +0200

    Proyecto Inicial

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject quil1 "1.0.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"] [quil "1.6.0"] ])
---
+++ test/quil1/test/core.clj
@@ -0,0 +1,6 @@
+(ns quil1.test.core
+  (:use [quil1.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
