ada162d3ec871a411d8e9854a482b818ba92bd39
juanantonioruz <juanantonioruz@gmail.com>
2012-09-28 12:01:42 +0200

    Quil configured for Live Editing

--- src/quil1/core.clj
+++ src/quil1/core.clj
@@ -1,1 +1,11 @@
-(ns quil1.core)
+;;https://github.com/quil/quil/wiki/Dynamic-Workflow-(for-generic-editors)
+(ns quil1.core
+  (:use quil.core)
+  (:require [quil1.ejemplo :as ejemplo]))
+
+(defsketch example                
+  :title "First Dynamic Example with Clojure/Quil=Clojure on top of Processing.org"
+  :setup ejemplo/setup           
+  :draw ejemplo/draw              
+  :size [523 500])                
+
