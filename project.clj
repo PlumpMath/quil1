da2a0b8d413b2a7cbed91d8c2ba0a521e65f5fc9
juanantonioruz <juanantonioruz@gmail.com>
2012-10-16 18:40:58 +0200

    Obtaining JSON from HTTP GET

--- project.clj
+++ project.clj
@@ -3,7 +3,12 @@
   :dependencies [
                   [org.clojure/clojure-contrib "1.2.0"] 
                  [org.clojure/clojure "1.4.0"]
-                 [quil "1.6.0"][clj-time "0.4.4"] ]
+                 [quil "1.6.0"][clj-time "0.4.4"]
+                 [org.clojure/data.json "0.2.0"]
+                 [com.cemerick/url "0.0.7"]
+                 [lein-swank "1.4.4"]
+                 [clj-http "0.5.5"]
+                 [cheshire "4.0.3"]]
   )
 
 
---
+++ src/mijson.clj
@@ -0,0 +1,14 @@
+(ns mijson
+  (:use [clojure.java.io])
+  (:require [clj-http.client :as client] [cheshire.core :as json])
+  )
+
+(def miurl "http://elections.huffingtonpost.com/pollster/api/polls.json?page=2")
+
+(defn parsea-ejemplo []
+  (json/parse-string (:body (client/get miurl))))
+
+(defn dame-valor-ejemplo
+  "probar con 'method'"
+  [clave]
+  ((first (parsea-ejemplo)) clave))
